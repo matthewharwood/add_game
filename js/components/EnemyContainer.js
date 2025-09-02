fb91174cb30e149a6aa2e11334f2eb8faaa96c1a
matthew harwood <matthhar12@gmail.com>
2025-09-01 20:41:23 -0700

    Updated ENemey Container

--- js/components/EnemyContainer.js
+++ js/components/EnemyContainer.js
@@ -11,21 +11,44 @@ class EnemyContainer extends HTMLElement {
   render() {
     this.shadowRoot.innerHTML = `
       <style>
+        @import url('/css/variables.css');
+
         :host {
           display: block;
-
           grid-row: 1 / 3;
           grid-column: 3 / 7;
+          padding: 24px 0 0 0;
+          box-sizing: border-box;
         }
 
-        .enemy-content {
-          text-align: center;
-          padding: 1rem;
+        .enemy-wrapper {
+          width: 100%;
           height: 100%;
+          border: 2px solid var(--color-border-default);
+          border-radius: 8px;
+          background-color: var(--color-bg-secondary);
+          box-shadow: var(--shadow-md);
           box-sizing: border-box;
-          display: flex;
-          align-items: center;
-          justify-content: center;
+        }
+
+
+        .flex-top-grid {
+            flex-direction: row;
+            display: flex;
+            width: 100%;
+            height: 90%;
+        }
+        .enemy-portrait {
+            width: 50%;
+        }
+        .enemy-info {
+            width: 50%;
+        }
+        .enemy-health {
+            height: 10%;
+            background-color: var(--color-bg-secondary);
+            width: 100%;
+            display: flex;
         }
 
         /* Tablet Styles */
@@ -44,8 +67,21 @@ class EnemyContainer extends HTMLElement {
           }
         }
       </style>
-      <div class="enemy-content">
-        <slot>Enemy</slot>
+      <div class="enemy-wrapper">
+
+            <div class="flex-top-grid">
+              <div class="enemy-portrait">
+
+              </div>
+              <div class="enemy-info">
+
+              </div>
+            </div>
+
+            <div class="enemy-health">
+                
+            </div>
+
       </div>
     `;
   }
